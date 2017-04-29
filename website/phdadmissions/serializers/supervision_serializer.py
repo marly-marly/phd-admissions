d1855eb94fec65247219808a277f968a1117e962
Marlyyyyy <mlt.residence@gmail.com>
2017-04-29 17:38:46 +0100

    Added the serializer class for comments.

--- website/phdadmissions/serializers/supervision_serializer.py
+++ website/phdadmissions/serializers/supervision_serializer.py
@@ -2,12 +2,15 @@ from rest_framework import serializers
 
 from authentication.serializers import AccountSerializer
 from phdadmissions.models.supervision import Supervision
+from phdadmissions.serializers.comment_serializer import CommentSerializer
 
 
 class SupervisionSerializer(serializers.ModelSerializer):
     supervisor = AccountSerializer
+    comments = CommentSerializer(many=True)
 
     class Meta:
         model = Supervision
         fields = (
-        'id', 'application', 'supervisor', 'acceptance_condition', 'recommendation', 'created_at', 'modified_at')
+            'id', 'application', 'supervisor', 'acceptance_condition', 'recommendation', 'created_at', 'modified_at',
+            "comments")
