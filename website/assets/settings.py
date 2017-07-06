f7e664ca52e84b9fdf0e543d327f2b5e2494aec9
Marlyyyyy <mlt.residence@gmail.com>
2017-07-06 15:03:28 +0100

    Added STATIC_ROOT value read from an environment variable to settings.py, so that the "collectstatic" Django command can be run at deployment.

--- website/assets/settings.py
+++ website/assets/settings.py
@@ -147,7 +147,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
-STATIC_ROOT = ''
+STATIC_ROOT = os.getenv("STATIC_ROOT", "")
 STATIC_URL = '/static/'
 STATIC_DIR = os.path.join(PROJECT_DIR, 'static')
 STATICFILES_DIRS = (
